d9849ef4f9ed83673c73c48a33be17f6c0670024
Mohamed BOUGHRARA <156963881+12001316@users.noreply.github.com>
2024-01-27 21:53:06 +0100

    Add files via upload

--- LireCommande.java
+++ LireCommande.java
@@ -1,5 +1,6 @@
 import java.io.*;
 
+//Déclaration de la classe qui permet la lecture du fichier des commandes.
 public class LireCommande{
   public static void main(String[] args) throws Exception {
    String MAKE;
@@ -13,56 +14,58 @@ public class LireCommande{
    String FJMP,argFJMP;
    String MARK,argMARK;
    String HALT;
-    File doc = new File("./Commandes.txt");
-
+   //Lecture fichier des commandes saisies par le joueur.
+   File doc = new File("./Commandes.txt");
+    
     BufferedReader obj = new BufferedReader(new FileReader(doc));
 
     String strng;
+    //Boucle while qui permet de parser le fichier à la recherche de la commande et de son argument.
     while ((strng = obj.readLine()) != null) 
     {
-    
-                                      
-                                        
-                                    switch(strng) {
-                                      case "LINK":
-                                                        System.out.println("\nC'est LINK.");
-                                                        break;
-                                      case "GRAB":
-                                                        System.out.println("\nC'est GRAB.");
-                                                        break;
-                                      case "DROP":
-                                                        System.out.println("\nC'est DROP.");
-                                                        break;
-                                      case "COPY":
-                                                        System.out.println("\nC'est COPY.");
-                                                        break;
-                                      case "MULI":
-                                                        System.out.println("\nC'est MULI.");
-                                                        break;
-                                      case "ADDI":
-                                                        System.out.println("\nC'est ADDI.");
-                                                        break;
-                                      case "SUBI":
-                                                        System.out.println("\nC'est SUBI.");
-                                                        break;
-                                      case "MAKE":
-                                                        System.out.println("\nC'est MAKE.");
-                                                        break;
-                                      case "MARK":
-                                                        System.out.println("\nC'est MARK.");
-                                                        break;
-                                      case "HALT":
-                                                        System.out.println("\nC'est HALT.");
-                                                        break;                   
-                                      case "FJMP":
-                                                        System.out.println("\nC'est FJMP.");
-                                                        break;
-                                      default:
-                                                        System.out.println("I don't know which one to order.");
-                                                        break;
-                                    }
-                                    System.out.println(strng);
-    }
+
+                                      switch(strng) {
+                                        case "LINK":
+                                                          System.out.println("\nCommande=LINK");
+                                                          break;
+                                        case "GRAB":
+                                                          System.out.println("\nCommande=GRAB");
+                                                          break;
+                                        case "DROP":
+                                                          System.out.println("\nCommande=DROP");
+                                                          break;
+                                        case "COPY":
+                                                          System.out.println("\nCommande=COPY");
+                                                          break;
+                                        case "MULI":
+                                                          System.out.println("\nCommande=MULI");
+                                                          break;
+                                        case "ADDI":
+                                                          System.out.println("\nCommande=ADDI");
+                                                          break;
+                                        case "SUBI":
+                                                          System.out.println("\nCommande=SUBI");
+                                                          break;
+                                        case "MAKE":
+                                                          System.out.println("\nCommande=MAKE");
+                                                          break;
+                                        case "MARK":
+                                                          System.out.println("\nCommande=MARK");
+                                                          break;
+                                        case "HALT":
+                                                          System.out.println("\nCommande=HALT");
+                                                          break;                   
+                                        case "FJMP":
+                                                          System.out.println("\nCommande=FJMP");
+                                                          break;
+                                        default:
+                                                          System.out.println("Commande="+strng.substring(0, 4)+" "+"Argument="+strng.substring(5, strng.length()));
+                                                          break;
+                                    }                             
+  }
+  //Affichage de chaque commande et de son argument s'il est présent.
+  //En cas de non présence de commande et d'argument on envoi null.
+  System.out.println(strng);
 }
 
 }
