1e49d61bde8275e396782a379f276e54705ee93e
Mohamed BOUGHRARA <156963881+12001316@users.noreply.github.com>
2024-02-19 21:48:26 +0100

    Add files via upload

---
+++ EcrireDansFichier.java
@@ -0,0 +1,26 @@
+package net.codejava.io;
+ 
+import java.io.FileWriter;
+import java.io.IOException;
+ 
+/**
+ * This program demonstrates how to write characters to a text file.
+ * @author www.codejava.net
+ *
+ */
+public class EcrireDansFichier {
+ 
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("./MonFichier.txt", true);
+            writer.write("Bonjour le Monde");
+            writer.write("\r\n");   // Ecrire une nouvelle ligne
+            writer.write("Bon chance!");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+ 
+    }
+ 
+}
